cb504d08ea89a7e5790dc90b8c6a28200889050a
deria10 <dev.deria@gmail.com>
2025-09-05 16:20:24 +0700

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,7 @@
-// eslint.config.js
-import js from "@eslint/js";
-import nextPlugin from "@next/eslint-plugin-next";
-
-/** @type {import("eslint").Linter.FlatConfig[]} */
+// eslint.config.mjs
 export default [
   {
-    ignores: ["**/.next/**", "**/node_modules/**", "**/dist/**"],
-  },
-  js.configs.recommended,
-  {
-    plugins: {
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      // custom rules kalau perlu
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-html-link-for-pages": "off",
-    },
+    ignores: ["**/.next/**", "**/node_modules/**"],
+    extends: ["next/core-web-vitals"],
   },
 ];
