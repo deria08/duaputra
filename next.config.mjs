18322aae3621d8fe3b0a260d34c12e1b7a1a79a1
deria10 <dev.deria@gmail.com>
2025-09-05 16:30:42 +0700

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   i18n: {
     locales: ["id", "en"], // bahasa yang tersedia
     defaultLocale: "id",   // bahasa default
